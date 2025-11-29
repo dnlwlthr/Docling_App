c707ad602a59fa456d3420f9cf029a59a8d70c74
dnlwlthr <daniel.walther85@gmx.de>
2025-11-29 09:54:13 +0100

    Initial Commit

---
+++ Docling_App/Docling_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Docling_AppApp.swift
+//  Docling_App
+//
+//  Created by Daniel Walther on 29.11.25.
+//
+
+import SwiftUI
+
+@main
+struct Docling_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
